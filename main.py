8f3a49b0a3658df2b655164909b27af17a13c7b7
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-11-13 18:43:28 -0800

    intial

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from datetime import datetime
 import s3fs
 
 
-access_key = "ZWRt6CP9UQTUk1o2RpZVuh9NA"
-access_secret = "089yhcMsoig2FmukPCQWnW6rZ0vV5SOPSmNFeyzB6C7mtrod5U"
-consumer_key = "1591523602586746881-PhiiXlw0pHAsJlW8csEqgSEWQu9Ev1"
-consumer_secret = "TId7WrdrqWlqJ5VgpoCl6haxup4AZT8UTWiz2hlYliFdP"
+access_key = "1591523602586746881-dNrJqEkt4DSn2i21jQcGqCizNVaUeh"
+access_secret = "PVIuEYL4MLFY3TbUja7yOBAXYoCJiOX9hLg66pEvkpQfk"
+consumer_key = "1MGkGkZ01bghv2WSRlCUUCvZA"
+consumer_secret = "BoRNSpx1dV7Xa8j9PfDzlCg7kfWrRrauEMTnvzCNPvE7HKpmXt"
 
-auth = tweepy.OAuthHandler(access_key, access_secret)
-auth.set_access_token(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
 
-api = tweepy.API(auth)
+api = tweepy.Client(auth)
 
-tweets = api.user_timeline(screen_name='@elonmusk',
-                           count=25,
-                           include_rts = False,
-                           tweet_mode= 'extended')
+tweets = api.get_users_tweets(id='@elonmusk')
 
 print(tweets)
+
+
 
 
 
